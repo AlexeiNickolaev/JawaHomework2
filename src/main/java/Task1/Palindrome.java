dec87e31577b30cd21f9e62fd93c8abdeaba0e88
Alexei <datlife2015@yandex.ru>
2023-03-21 17:06:08 +0300

    Homework2

---
+++ src/main/java/Task1/Palindrome.java
@@ -0,0 +1,24 @@
+package Task1;
+
+// Напишите метод, который принимает на вход строку (String)
+// и определяет является ли строка палиндромом (возвращает boolean значение).
+
+
+public class Palindrome{
+    public static void main(String[] args) {
+        String str = "level";
+
+        boolean isPalindrome = isPalindrome(str);
+
+        if (isPalindrome) {
+            System.out.println(str + " является палиндромом");
+        } else {
+            System.out.println(str + " не является палиндромом");
+        }
+    }
+
+    public static boolean isPalindrome(String str) {
+        String reverseStr = new StringBuilder(str).reverse().toString();
+        return str.equals(reverseStr);
+    }
+}
